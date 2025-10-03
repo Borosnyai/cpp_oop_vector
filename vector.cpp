5affc5eaafcf8e31f6cd301473fa660c116b9ca7
Borosnyai <borosnyaiv@gmail.com>
2025-10-03 17:39:10 +0200

    Final Solution

--- vector.cpp
+++ vector.cpp
@@ -1,1 +1,80 @@
+
+
+
+
 #include "vector.hpp"
+#include<cmath>
+
+namespace geometry{
+  Vector::Vector(){
+    x = 0.0;
+    y = 0.0;
+    z = 0.0;
+  }
+
+  Vector::Vector(double x, double y, double z){
+    this->x = x;
+    this->y = y;
+    this->z = z;
+  }
+
+  // Vector: Függvenynek milyen tipusu a visszateresi erteke
+  // 2Vector:: = A Vector osztalybol valositunk meg egy függveny.
+  // plus = A függveny neve
+  // const a parameter listaban, azt jelenti a kapott adat, nem valtozhat meg.
+  // Vector& = Amit kapunk annak mi a tipusa es az & jel közvetlen hozzaferest jelenti.Ez egy pointer, amit refericianalk neveznek!
+  // az o betü = a masik Vector neve (other)
+  // const függvenyen kivül = Függveny nem valtoztatja meg az osztaly adatait.
+
+  Vector Vector::plus(const Vector& o) const{
+    /*Vector result;
+    result.x = this->x + o.x;
+    result.y = this->y + o.y;
+    result.z = this->z + o.z;*/
+    return Vector(this->x + o.x,this->y + o.y,this->z + o.z);
+  }
+  Vector Vector::minus(const Vector& o) const{
+
+    return Vector(this->x - o.x,this->y - o.y,this->z - o.z);
+  }
+
+  Vector Vector::times(double s) const{
+
+
+    return Vector(this->x * s, this->y * s, this->z * s);
+  }
+  Vector Vector::cross(const Vector& o) const{
+
+    return Vector(
+      this->y * o.z - this->z * o.y,
+     this->z * o.x - this->x * o.z,
+     this->x * o.y - this->y * o.x
+    );
+  }
+
+  // Scalar operations - return numbers
+  double Vector::times(const Vector& o) const{
+    double result = this->x * o.x + this->y * o.y + this->z * o.z;
+    return result;
+  }
+  double Vector::dot (const Vector& o) const{
+    double result = this->x * o.x + this->y * o.y + this->z * o.z;
+    return result;
+
+  }
+  double Vector::length() const{
+
+
+    return sqrt(x * x + y * y + z * z);
+  }
+
+  double Vector::magnitude () const{
+
+    return length();
+  }
+  // specialis kimeneti folyam, ami lehet file vagy console output.
+  void Vector::print(std::ostream& o) const{
+    o << "(" << x << ", " << y << ", " << z << ")";
+  }
+
+}
